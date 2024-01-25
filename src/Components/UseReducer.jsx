177878c3cb7081abf8b1a3b9156e5f60a08f5352
Dheeraj Singh <94373786+Dheerajsingh002@users.noreply.github.com>
2024-01-25 21:17:49 +0530

    usereducer

--- src/Components/UseReducer.jsx
+++ src/Components/UseReducer.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 const reducerfn=(state,action)=>{
   switch(action.type)
   {
@@ -6,6 +6,8 @@ const reducerfn=(state,action)=>{
       return {steps: state.steps+1};
     case "BACKWARD":
       return {steps: state.steps-1};
+    case "CUSTOM":
+      return {steps: state.steps+ action.payload}
     default:
       return {...state };
   }
@@ -15,6 +17,7 @@ const initialstate={
   steps:0
 }
 function UseReducer() {
+  const [inputvalue,setInput]=useState("");
   const[state,dispatch]=useReducer(reducerfn,initialstate);
   const handlebackward=()=>
   { 
@@ -27,6 +30,12 @@ function UseReducer() {
     dispatch(action)
 
   }
+  const handlecustom=(setsteps)=>
+    {
+      const action={type:"CUSTOM",payload:Number(setsteps)};
+      dispatch(action);
+
+    }
   return (
     <div>
       <h1>useReducer</h1>
@@ -34,7 +43,10 @@ function UseReducer() {
       <div style={{display:"flex",justifyContent:"center",alignItems:"center",gap:10}}>
         <button onClick={handlefordward}>Move Fordward</button>
         <button onClick={handlebackward}>Move Backward</button>
-
+      </div>
+      <div>
+        <input type="text" value={inputvalue} onChange={(e)=> setInput(e.target.value)} />
+        <button onClick={()=>handlecustom(Number(inputvalue))}>custom increment</button>
       </div>
     </div>
   )
